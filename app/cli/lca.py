bb3c7661b813dfa112f660bc6969f9b8f91e4265
Anne-Lore Montagne (am279929) <anne-lore.montagne@cea.fr>
2024-12-17 13:56:21 +0100

    (feat) add --init option to the cli

--- app/cli/lca.py
+++ app/cli/lca.py
@@ -1,46 +1,57 @@
+from typing import Annotated
+from typing import Optional
+
 import typer
 import yaml
 
 from appabuild.model.builder import ImpactModelBuilder
 from appabuild.setup import project_setup
+from docopt import Argument
 
 app = typer.Typer()
 
 
 @app.command()
-def init_build(appalca_config_path: str, lca_config_path: str):
+def build(lca_config_path: Annotated[str, typer.Argument(help="LCA configuration file")],
+          appabuild_config_path: Annotated[Optional[str], typer.Argument(
+              help="AppaBuild environment configuration file, required unless --no-init is specified")] = None,
+          init: Annotated[bool, typer.Option(help="initialize AppaBuild environment")] = True):
     """
-    Initialize a Brightway environment (background and foreground databases).
-    Build an impact model and save it to the disk
+    Build an impact model and save it to the disk.
+    An AppaBuild environment is initialized (background and foreground databases), unless --no-init is specified.
+
     """
-    init(appalca_config_path)
-    build(lca_config_path)
+    if init is True:
+        if not appabuild_config_path:
+            print("AppaBuild configuration file and LCA configuration file are required for initialization")
+            return
+        initialize(appabuild_config_path)
+
+    do_build(lca_config_path)
 
 
-@app.command()
-def init(appalca_config_path: str):
+def initialize(appabuild_config_path: str):
     """
     Initialize a Brightway environment (background and foreground databases).
-    :param appalca_config_path: generic information required by Appa LCA to be initialized, such
+    :param appabuild_config_path: generic information required by Appa Build to be initialized, such
     as location of EcoInvent or name of Brightway project. This config file should
     remain the same for all your LCAs.
     """
-    with open(appalca_config_path, "r") as stream:
-        appalca_config = yaml.safe_load(stream)
+    with open(appabuild_config_path, "r") as stream:
+        appabuild_config = yaml.safe_load(stream)
 
     project_setup(
-        project_name=appalca_config["project_name"],
-        ecoinvent_name=appalca_config["databases"]["ecoinvent"]["name"],
-        ecoinvent_path=appalca_config["databases"]["ecoinvent"]["path"],
-        foreground_name=appalca_config["databases"]["foreground"]["name"],
-        foreground_path=appalca_config["databases"]["foreground"]["path"],
-        fu_name=appalca_config["databases"]["foreground"]["fu_activity"],
-        parameters=appalca_config["parameters"],
+        project_name=appabuild_config["project_name"],
+        ecoinvent_name=appabuild_config["databases"]["ecoinvent"]["name"],
+        ecoinvent_path=appabuild_config["databases"]["ecoinvent"]["path"],
+        foreground_name=appabuild_config["databases"]["foreground"]["name"],
+        foreground_path=appabuild_config["databases"]["foreground"]["path"],
+        fu_name=appabuild_config["databases"]["foreground"]["fu_activity"],
+        parameters=appabuild_config["parameters"],
     )
 
 
-@app.command()
-def build(lca_config_path: str):
+def do_build(lca_config_path: str):
     """
     Build an impact model and save it to the disk (to the location configured in the file).
     :param lca_config_path: information about the current LCA, such as functional unit,
