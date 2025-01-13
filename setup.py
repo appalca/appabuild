c314668a0a70f6b02fbcbc84ed9ec9570acc344d
Anne-Lore Montagne (am279929) <anne-lore.montagne@cea.fr>
2025-01-13 08:59:04 +0100

    (update) bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="appabuild",
-    version="0.2.0",
+    version="0.3.0",
     author="Maxime Peralta",
     author_email="maxime.peralta@cea.fr",
     description="Appa Build is a package to build impact models",
