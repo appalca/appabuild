8d24984d0302707690a770aa063d01e6d8cd87f0
BossyRomain <romain.bossy@cea.fr>
2025-02-07 16:35:23 +0100

    Modification structure du dossier de test

--- tests/end_to_end_tests/test_build.py
+++ tests/end_to_end_tests/test_build.py
@@ -18,22 +18,24 @@ def compare_files_line_by_line(file1, file2):
 
 
 def test_build_command():
+    appaconf_file = "test_build/appalca_conf.yaml"
+    conf_file = "test_build/nvidia_ai_gpu_chip_lca_conf.yaml"
+    expected_file = "test_build/nvidia_ai_gpu_chip_lca_conf.yaml"
+
     result = subprocess.run(
         [
             "appabuild",
             "lca",
             "build",
-            "./appalca_conf.yaml",
-            "nvidia_ai_gpu_chip_lca_conf.yaml",
+            appaconf_file,
+            conf_file,
         ],
         capture_output=True,
         text=True,
     )
     assert result.returncode == 0
 
-    result = compare_files_line_by_line(
-        "nvidia_ai_gpu_chip_expected.yaml", "nvidia_ai_gpu_chip.yaml"
-    )
+    result = compare_files_line_by_line(expected_file, "nvidia_ai_gpu_chip.yaml")
 
     os.remove("nvidia_ai_gpu_chip.yaml")
 
