f61dc3ab89d4b568cfe50c6bdcf612765ac770b1
BossyRomain <romain.bossy@cea.fr>
2025-02-07 16:28:58 +0100

    Add a missing pip install in the workflow

---
+++ tests/end_to_end_tests/build/test_build.py
@@ -0,0 +1,40 @@
+"""
+Test that the command appabuild lca build works correctly with a simple example.
+"""
+
+import os
+import subprocess
+
+
+def compare_files_line_by_line(file1, file2):
+    with (
+        open(file1, "r") as expected_file,
+        open(file2, "r") as result_file,
+    ):
+        for line1, line2 in zip(expected_file, result_file):
+            if line1 != line2:
+                return False
+    return True
+
+
+def test_build_command():
+    result = subprocess.run(
+        [
+            "appabuild",
+            "lca",
+            "build",
+            "./appalca_conf.yaml",
+            "nvidia_ai_gpu_chip_lca_conf.yaml",
+        ],
+        capture_output=True,
+        text=True,
+    )
+    assert result.returncode == 0
+
+    result = compare_files_line_by_line(
+        "nvidia_ai_gpu_chip_expected.yaml", "nvidia_ai_gpu_chip.yaml"
+    )
+
+    os.remove("nvidia_ai_gpu_chip.yaml")
+
+    assert result is True
