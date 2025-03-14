a83a905901c3dbacb99bf1d7b39273719fa04b9e
Bossy-Romain <romain.bossy@cea.fr>
2025-03-14 10:46:40 +0100

    (feat) Update the project structure (#18)

--- appabuild/cli/main.py
+++ appabuild/cli/main.py
@@ -1,7 +1,6 @@
+import typer
 from app.cli.database import app as database_app
 from app.cli.lca import app as lca_app
-import typer
-
 
 cli_app = typer.Typer()
 cli_app.add_typer(database_app, name="database")
