792f6e2616630a70d441245879fbe510e86d4ca9
Bossy-Romain <romain.bossy@cea.fr>
2025-03-20 15:45:07 +0100

    (feat) Add the CI (#16)

--- appabuild/cli/main.py
+++ appabuild/cli/main.py
@@ -1,6 +1,7 @@
 import typer
-from app.cli.database import app as database_app
-from app.cli.lca import app as lca_app
+
+from appabuild.cli.database import app as database_app
+from appabuild.cli.lca import app as lca_app
 
 cli_app = typer.Typer()
 cli_app.add_typer(database_app, name="database")
