a98ef42cf5e1163044d142e736401acff5ae28d6
BossyRomain <romain.bossy@cea.fr>
2025-05-05 15:21:31 +0200

    (feat) Add options for the command graph

--- appabuild/cli/lca.py
+++ appabuild/cli/lca.py
@@ -37,6 +37,18 @@ def build(
     setup.build(lca_config_path, foreground_database)
 
 
+def validate_type(type: str) -> str:
+    if type not in ["png", "svg"]:
+        raise typer.BadParameter("Only png or svg allowed")
+    return type
+
+
+def validate_size(size: int) -> int:
+    if size <= 0:
+        raise typer.BadParameter("must be superior to 0")
+    return size
+
+
 @app.command()
 def graph(
     path: Annotated[str, typer.Argument(help="Root path of foreground datasets")],
@@ -44,13 +56,28 @@ def graph(
         str,
         typer.Argument(help="Name of the root dataset (without its file extension)"),
     ],
+    type: Annotated[
+        str, typer.Option(help="Output an image in PNG format", callback=validate_type)
+    ] = "png",
+    width: Annotated[
+        int, typer.Option(help="Width of the output image", callback=validate_size)
+    ] = 750,
+    height: Annotated[
+        int, typer.Option(help="Height of the output image", callback=validate_size)
+    ] = 750,
 ):
     """
     Generate a mermaid graph from a set of foreground datasets and export it in an image file (SVG format).
     :param path: root path of the foreground datasets used to build the graph.
     :param fu_name: name of the dataset that will be the root of the graph.
+    :param type: type of the output image, can only be png or svg, the default value is png.
+    :param width: width of the output image.
+    :param height: height of the output image.
 
     """
     mermaid_graph = build_mermaid_graph(path, fu_name)
-    render = md.Mermaid(mermaid_graph)
-    render.to_svg(fu_name + ".svg")
+    render = md.Mermaid(mermaid_graph, width=width, height=height)
+    if type == "svg":
+        render.to_svg(fu_name + ".svg")
+    elif type == "png":
+        render.to_png(fu_name + ".png")
